01638442c640d0c5db3a6882cef3abb9be92d0e4
Rafaela Bibiano <rafaela.bibiano@cassotis.com>
2026-05-17 21:12:03 -0300

    multistart and multistar + packing cstr

--- gurobi.py
+++ gurobi.py
@@ -112,50 +112,259 @@ for S_tuple in combinations(clientes, 2):
 # # Packing para S = {1,4,5,6,7,9,10}
 # # ============================================================
 
-# S1 = {1, 4, 5, 6, 7, 9, 10}
+S1 = {1, 4, 5, 6, 7, 9, 10}
 
-# model.addConstr(
-#     gp.quicksum(
-#         x[i, j]
-#         for i in S1
-#         for j in S1
-#         if i != j
-#     ) <= len(S1) - math.ceil(len(S1) / K),
-#     name="packing_S_1_4_5_6_7_9_10"
-# )
+model.addConstr(
+    gp.quicksum(
+        x[i, j]
+        for i in S1
+        for j in S1
+        if i != j
+    ) <= len(S1) - math.ceil(len(S1) / K),
+    name="packing_S_1_4_5_6_7_9_10"
+)
 
 
 # # ============================================================
 # # Packing para S = {1,5,4,10}
 # # ============================================================
 
-# S2 = {1, 5, 4, 10}
+S2 = {1, 5, 4, 10}
 
-# model.addConstr(
-#     gp.quicksum(
-#         x[i, j]
-#         for i in S2
-#         for j in S2
-#         if i != j
-#     ) <= len(S2) - math.ceil(len(S2) / K),
-#     name="packing_S_1_5_4_10"
-# )
+model.addConstr(
+    gp.quicksum(
+        x[i, j]
+        for i in S2
+        for j in S2
+        if i != j
+    ) <= len(S2) - math.ceil(len(S2) / K),
+    name="packing_S_1_5_4_10"
+)
 
 # # ============================================================
 # # Packing para S = {9,4,3,7,10}
 # # ============================================================
 
-# S = {9, 4, 3, 7, 10}
+S3 = {9, 4, 3, 7, 10}
+
+model.addConstr(
+    gp.quicksum(
+        x[i, j]
+        for i in S3
+        for j in S3
+        if i != j
+    ) <= len(S3) - math.ceil(len(S3) / K),
+    name="packing_S_9_4_3_7_10"
+)
+
+# # ============================================================
+# # LP2
+# # Packing para S = {1,2,6,7,9}
+# # ============================================================
+
+# S4 = {1, 2, 6, 7, 9}
 
 # model.addConstr(
 #     gp.quicksum(
 #         x[i, j]
-#         for i in S
-#         for j in S
+#         for i in S4
+#         for j in S4
 #         if i != j
-#     ) <= len(S) - math.ceil(len(S) / K),
-#     name="packing_S_9_4_3_7_10"
+#     ) <= len(S4) - math.ceil(len(S4) / K),
+#     name="packing_S_1_2_6_7_9"
 # )
+
+# # ============================================================
+# # LP2
+# # Packing para S = {3,4,5,10}
+# # ============================================================
+
+S5 = {3, 4, 5, 10}
+
+model.addConstr(
+    gp.quicksum(
+        x[i, j]
+        for i in S5
+        for j in S5
+        if i != j
+    ) <= len(S5) - math.ceil(len(S5) / K),
+    name="packing_S_3_4_5_10"
+)
+
+# # ============================================================
+# # LP2
+# # Packing para S = {1,2,3,4,5,6,7,9,10}
+# # ============================================================
+
+# S6 = {1, 2, 3, 4, 5, 6, 7, 9, 10}
+
+# model.addConstr(
+#     gp.quicksum(
+#         x[i, j]
+#         for i in S6
+#         for j in S6
+#         if i != j
+#     ) <= len(S6) - math.ceil(len(S6) / K),
+#     name="packing_S_1_2_3_4_5_6_7_9_10"
+# )
+
+# # ============================================================
+# # LP3
+# # Packing para S = {2,3,4,5,7,8,10}
+# # ============================================================
+
+# S7 = {2, 3, 4, 5, 7, 8, 10}
+
+# model.addConstr(
+#     gp.quicksum(
+#         x[i, j]
+#         for i in S7
+#         for j in S7
+#         if i != j
+#     ) <= len(S7) - math.ceil(len(S7) / K),
+#     name="packing_S_2_3_4_5_7_8_10"
+# )
+
+# # ============================================================
+# # LP3 - packing + multistar
+# # Packing para S = {2,4,5,10}
+# # ============================================================
+
+S8 = {2, 4, 5, 10}
+
+model.addConstr(
+    gp.quicksum(
+        x[i, j]
+        for i in S8
+        for j in S8
+        if i != j
+    ) <= len(S8) - math.ceil(len(S8) / K),
+    name="packing_S_2_4_5_10"
+)
+
+# # ============================================================
+# # LP4
+# # Packing para S = {1,2,6,9}
+# # ============================================================
+
+S9 = {1, 2, 6, 9}
+
+model.addConstr(
+    gp.quicksum(
+        x[i, j]
+        for i in S9
+        for j in S9
+        if i != j
+    ) <= len(S9) - math.ceil(len(S9) / K),
+    name="packing_S_1_2_6_9"
+)
+
+# # ============================================================
+# # LP5
+# # Packing para S = {3,7,8,9}
+# # ============================================================
+
+S10 = {3, 7, 8, 9}
+
+model.addConstr(
+    gp.quicksum(
+        x[i, j]
+        for i in S10
+        for j in S10
+        if i != j
+    ) <= len(S10) - math.ceil(len(S10) / K),
+    name="packing_S_3_7_8_9"
+)
+
+# # ============================================================
+# # LP6
+# # Packing para S = {4,5,8,10}
+# # ============================================================
+
+# S11 = {4, 5, 8, 10}
+
+# model.addConstr(
+#     gp.quicksum(
+#         x[i, j]
+#         for i in S11
+#         for j in S11
+#         if i != j
+#     ) <= len(S11) - math.ceil(len(S11) / K),
+#     name="packing_S_4_5_8_10"
+# )
+
+# # ============================================================
+# # LP3 - packing + multistar
+# # Packing para S = {3,4,5,8,10}
+# # ============================================================
+
+S12 = {3, 4, 5, 8, 10}
+
+model.addConstr(
+    gp.quicksum(
+        x[i, j]
+        for i in S12    
+        for j in S12
+        if i != j
+    ) <= len(S12) - math.ceil(len(S12) / K),
+    name="packing_S_3_4_5_8_10"
+)
+
+# # ============================================================
+# # LP4 - packing + multistar
+# # Packing para S = {2,3,7,8}
+# # ============================================================
+
+# S13 = {2, 3, 7, 8}
+
+# model.addConstr(
+#     gp.quicksum(
+#         x[i, j]
+#         for i in S13 
+#         for j in S13
+#         if i != j
+#     ) <= len(S13) - math.ceil(len(S13) / K),
+#     name="packing_S_2_3_7_8"
+# )
+
+# # ============================================================
+# # LP4 - packing + multistar
+# # Packing para S = {4,5,7,10}
+# # ============================================================
+
+S14 = {4, 5, 7, 10}
+
+model.addConstr(
+    gp.quicksum(
+        x[i, j]
+        for i in S14 
+        for j in S14
+        if i != j
+    ) <= len(S14) - math.ceil(len(S14) / K),
+    name="packing_S_4_5_7_10"
+)
+
+# # ============================================================
+# # LP4 - packing + multistar
+# # Packing para S = {2,3,6,7,8,9}
+# # ============================================================
+
+S15 = {2, 3, 6, 7, 8, 9}
+
+model.addConstr(
+    gp.quicksum(
+        x[i, j]
+        for i in S15 
+        for j in S15
+        if i != j
+    ) <= len(S15) - math.ceil(len(S15) / K),
+    name="packing_S_2_3_6_7_8_9"
+)
+
+
+
+# ---------------------------------------------------- MULTISTAR -------------------------
+
 
 # ============================================================
 # Multistar para S = {1,4,5,6,7,9,10}
@@ -251,6 +460,281 @@ model.addConstr(
     )
     <= len(S4) * (K - 1),
     name="multistar_S_1_2_5"
+)
+
+# ============================================================
+# LP2
+# Multistar para S = {3,4,5,10}
+# ============================================================
+
+# S5 = {3, 4, 5, 10}
+# fora_S5 = set(clientes) - S5
+
+# model.addConstr(
+#     K * gp.quicksum(
+#         x[i, j]
+#         for i in S5
+#         for j in S5
+#         if i != j
+#     )
+#     +
+#     gp.quicksum(
+#         x[i, j]
+#         for i in S5
+#         for j in fora_S5
+#     )
+#     <= len(S5) * (K - 1),
+#     name="multistar_S_3_4_5_10"
+# )
+
+# ============================================================
+# LP2
+# Multistar para S = {6,7,9}
+# ============================================================
+
+S6 = {6, 7, 9}
+fora_S6 = set(clientes) - S6
+
+model.addConstr(
+    K * gp.quicksum(
+        x[i, j]
+        for i in S6
+        for j in S6
+        if i != j
+    )
+    +
+    gp.quicksum(
+        x[i, j]
+        for i in S6
+        for j in fora_S6
+    )
+    <= len(S6) * (K - 1),
+    name="multistar_S_6_7_9"
+)
+
+# ============================================================
+# LP2
+# Multistar para S = {1,2,6,9}
+# ============================================================
+
+# S7 = {1, 2, 6, 9}
+# fora_S7 = set(clientes) - S7
+
+# model.addConstr(
+#     K * gp.quicksum(
+#         x[i, j]
+#         for i in S7
+#         for j in S7
+#         if i != j
+#     )
+#     +
+#     gp.quicksum(
+#         x[i, j]
+#         for i in S7
+#         for j in fora_S7
+#     )
+#     <= len(S7) * (K - 1),
+#     name="multistar_S_1_2_6_9"
+# )
+
+# ============================================================
+# LP3
+# Multistar para S = {3,4,5,7,10}
+# ============================================================
+
+# S8 = {3,4,5,7,10}
+# fora_S8 = set(clientes) - S8
+
+# model.addConstr(
+#     K * gp.quicksum(
+#         x[i, j]
+#         for i in S8
+#         for j in S8
+#         if i != j
+#     )
+#     +
+#     gp.quicksum(
+#         x[i, j]
+#         for i in S8
+#         for j in fora_S8
+#     )
+#     <= len(S8) * (K - 1),
+#     name="multistar_S_3_4_5_7_10"
+# )
+
+# ============================================================
+# LP3
+# Multistar para S = {1,2,6,7,9}
+# ============================================================
+
+S9 = {1, 2, 6, 7, 9}
+fora_S9 = set(clientes) - S9
+
+model.addConstr(
+    K * gp.quicksum(
+        x[i, j]
+        for i in S9
+        for j in S9
+        if i != j
+    )
+    +
+    gp.quicksum(
+        x[i, j]
+        for i in S9
+        for j in fora_S9
+    )
+    <= len(S9) * (K - 1),
+    name="multistar_S_1_2_6_7_9"
+)
+
+# ============================================================
+# LP3
+# Multistar para S = {2,4,5,6,7,9,10}
+# ============================================================
+
+# S10 = {2, 4, 5, 6, 7, 9, 10}
+# fora_S10 = set(clientes) - S10
+
+# model.addConstr(
+#     K * gp.quicksum(
+#         x[i, j]
+#         for i in S10
+#         for j in S10
+#         if i != j
+#     )
+#     +
+#     gp.quicksum(
+#         x[i, j]
+#         for i in S10
+#         for j in fora_S10
+#     )
+#     <= len(S10) * (K - 1),
+#     name="multistar_S_2_4_5_6_7_9_10"
+# )
+
+# ============================================================
+# LP2 - packing + multistar
+# Multistar para S = {2,4,5,10}
+# ============================================================
+
+S11 = {2, 4, 5, 10}
+fora_S11 = set(clientes) - S11
+
+model.addConstr(
+    K * gp.quicksum(
+        x[i, j]
+        for i in S11 
+        for j in S11
+        if i != j
+    )
+    +
+    gp.quicksum(
+        x[i, j]
+        for i in S11
+        for j in fora_S11
+    )
+    <= len(S11) * (K - 1),
+    name="multistar_S_2_4_5_10"
+)
+
+# ============================================================
+# LP3 - packing + multistar
+# Multistar para S = {3,4,5,8,10}
+# ============================================================
+
+S12 = {3, 4, 5, 8, 10}
+fora_S12 = set(clientes) - S12
+
+model.addConstr(
+    K * gp.quicksum(
+        x[i, j]
+        for i in S12 
+        for j in S12
+        if i != j
+    )
+    +
+    gp.quicksum(
+        x[i, j]
+        for i in S12
+        for j in fora_S12
+    )
+    <= len(S12) * (K - 1),
+    name="multistar_S_3_4_5_8_10"
+)
+
+# ============================================================
+# LP4 - packing + multistar
+# Multistar para S = {2,3,6,7,8,9}
+# ============================================================
+
+S13 = {2, 3, 6, 7, 8, 9}
+fora_S13 = set(clientes) - S13
+
+model.addConstr(
+    K * gp.quicksum(
+        x[i, j]
+        for i in S13 
+        for j in S13
+        if i != j
+    )
+    +
+    gp.quicksum(
+        x[i, j]
+        for i in S13
+        for j in fora_S13
+    )
+    <= len(S13) * (K - 1),
+    name="multistar_S_2_3_6_7_8_9"
+)
+
+# ============================================================
+# LP4 - packing + multistar
+# Multistar para S = {4,5,7,10}
+# ============================================================
+
+S14 = {4, 5, 7, 10}
+fora_S14 = set(clientes) - S14
+
+model.addConstr(
+    K * gp.quicksum(
+        x[i, j]
+        for i in S14 
+        for j in S14
+        if i != j
+    )
+    +
+    gp.quicksum(
+        x[i, j]
+        for i in S14
+        for j in fora_S14
+    )
+    <= len(S14) * (K - 1),
+    name="multistar_S_4_5_7_10"
+)
+
+# ============================================================
+# LP5 - packing + multistar
+# Multistar para S = {3,7,8,9}
+# ============================================================
+
+S15 = {3, 7, 8, 9}
+fora_S15 = set(clientes) - S15
+
+model.addConstr(
+    K * gp.quicksum(
+        x[i, j]
+        for i in S15 
+        for j in S15
+        if i != j
+    )
+    +
+    gp.quicksum(
+        x[i, j]
+        for i in S15
+        for j in fora_S15
+    )
+    <= len(S15) * (K - 1),
+    name="multistar_S_3_7_8_9"
 )
 
 # ============================================================
